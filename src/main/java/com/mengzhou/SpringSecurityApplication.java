24f0634ebc1ac43e661ffe23f6b28167f7a3cc81
meng <mengzhou44@gmail.com>
2025-05-18 11:35:50 -0600

    init commit

--- src/main/java/com/mengzhou/SpringSecurityApplication.java
+++ src/main/java/com/mengzhou/SpringSecurityApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringDataApplication {
+public class SpringSecurityApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpringDataApplication.class, args);
+        SpringApplication.run(SpringSecurityApplication.class, args);
     }
 }
